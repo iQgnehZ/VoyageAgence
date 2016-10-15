87c3216f7b569990c75f12f645de738dc57cf58b
郑琪 <qizheng1994@outlook.com>
2016-10-15 20:41:56 +0200

    添加etape 和 programmation 的修改链接

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -163,4 +163,126 @@ class UserController extends Controller
 		));
 		 
 	}
+	/**
+	 * @Route("/circuit/etape/delete/{circuit_id}/{id}", name="etape_delete" )
+	 */
+	public function deleteEtapeAction($circuit_id,$id)
+	{
+		$em = $this->getDoctrine()->getManager();
+		$circuit=$em->getRepository('AppBundle:Circuit')->find($circuit_id);
+		$etape=$em->getRepository('AppBundle:Etape')->find($id);
+		
+		$circuit->removeEtape($etape);
+		$em->remove($etape);
+		$em->flush();
+	
+		$this->addFlash(
+				'notice',
+				'Etape Removed'
+				);
+	
+		return $this->redirectToRoute('circuit_details',array('id' => $circuit_id));
+		
+	}
+	/**
+	 * @Route("/circuit/create", name="circuit_create")
+	 */
+	public function createAction(Request $request)
+	{
+		$circuit = new Circuit;
+		$form = $this->createFormBuilder($circuit)
+		->add('pays_depart', TextType::class, array('attr' => array('class' =>'form-control','style' =>'margin-bottom:15px')))
+		->add('ville_depart', TextType::class, array('attr' => array('class' =>'form-control','style' =>'margin-bottom:15px')))
+		->add('ville_arrivee', TextType::class, array('attr' => array('class' =>'form-control','style' =>'margin-bottom:15px')))
+		->add('duree_circuit', IntegerType::class, array('attr' => array('class' =>'formcontrol','style' =>'margin-bottom:15px')))
+		->add('description', TextType::class, array('attr' => array('class' =>'form-control','style' =>'margin-bottom:15px')))
+		->add('save', SubmitType::class, array('label'=>'Create Circuit','attr' => array('class' =>'btn btn_primary','style' =>'margin-bottom:15px')))
+		->getForm();
+		$form->handleRequest($request);
+	
+	
+		if($form->isSubmitted() && $form->isValid()){
+			$paysDepart = $form['pays_depart']->getData();
+			$villeDepart = $form['ville_depart']->getData();
+			$villeArrivee = $form['ville_arrivee']->getData();
+			$description = $form['description']->getData();
+			$dureeCircuit = $form['duree_circuit']->getData();
+	
+			$circuit->setDescription($description);
+			$circuit->setDureeCircuit($dureeCircuit);
+			$circuit->setPaysDepart($paysDepart);
+			$circuit->setVilleArrivee($villeArrivee);
+			$circuit->setVilleDepart($villeDepart);
+	
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($circuit);
+			$em->flush();
+	
+			$this->addFlash('notice','Circuit Added');
+	
+			return $this->redirectToRoute('circuits_list');
+		}
+		return $this->render('user/create.html.twig',array(
+				'form'=> $form->createView()
+		));
+	}
+	/**
+	 * @Route("/circuit/edit/{id}", name="circuit_edit" )
+	 */
+	public function editAction($id, Request $request)
+	{
+		$em = $this->getDoctrine()->getManager();
+		$circuit=$em->getRepository('AppBundle:Circuit')->find($id);
+	
+		$circuit->setDescription($circuit->getDescription());
+		$circuit->setDureeCircuit($circuit->getdureeCircuit());
+		$circuit->setPaysDepart($circuit->getpaysDepart());
+		$circuit->setVilleArrivee($circuit->getvilleArrivee());
+		$circuit->setVilleDepart($circuit->getvilleDepart());
+	
+		$form = $this->createFormBuilder($circuit)
+		->add('pays_depart', TextType::class, array('attr' => array('class' =>'form-control','style' =>'margin-bottom:15px')))
+		->add('ville_depart', TextType::class, array('attr' => array('class' =>'form-control','style' =>'margin-bottom:15px')))
+		->add('ville_arrivee', TextType::class, array('attr' => array('class' =>'form-control','style' =>'margin-bottom:15px')))
+		->add('duree_circuit', IntegerType::class, array('attr' => array('class' =>'formcontrol','style' =>'margin-bottom:15px')))
+		->add('description', TextType::class, array('attr' => array('class' =>'form-control','style' =>'margin-bottom:15px')))
+		->add('save', SubmitType::class, array('label'=>'Update Circuit','attr' => array('class' =>'btn btn_primary','style' =>'margin-bottom:15px')))
+		->getForm();
+	
+		$form->handleRequest($request);
+	
+		if($form->isSubmitted() && $form->isValid()){
+			$paysDepart = $form['pays_depart']->getData();
+			$villeDepart = $form['ville_depart']->getData();
+			$villeArrivee = $form['ville_arrivee']->getData();
+			$dureeCircuit = $form['duree_circuit']->getData();
+			$description = $form['description']->getData();
+	
+			$em = $this->getDoctrine()->getManager();
+			$circuit = $em->getRepository('AppBundle:Circuit')->find($id);
+	
+			$circuit->setDescription($description);
+			$circuit->setDureeCircuit($dureeCircuit);
+			$circuit->setPaysDepart($paysDepart);
+			$circuit->setVilleArrivee($villeArrivee);
+			$circuit->setVilleDepart($villeDepart);
+	
+			$em->persist($circuit);
+			$em->flush();
+			$this->addFlash(
+					'notice',
+					'Circuit Updated'
+					);
+	
+			return $this->redirectToRoute('circuits_list');
+		}
+	
+		return $this->render('user/edit.html.twig', array(
+				'circuit' => $circuit,
+				'form' => $form->createView()
+	
+		));
+			
+	}
+	
 }
